2c335b3882aeabfbcdbbbb8465b5366f278ae494
huangmin <hmiter@sina.com>
2016-07-09 21:18:17 +0800

    d

--- src/main/java/com/xinfan/wxshop/business/admin/FilmAction.java
+++ src/main/java/com/xinfan/wxshop/business/admin/FilmAction.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.xinfan.wxshop.business.entity.Film;
 import com.xinfan.wxshop.business.model.DataTableDataGrid;
 import com.xinfan.wxshop.business.model.JSONResult;
 import com.xinfan.wxshop.business.service.FilmService;
@@ -71,6 +72,12 @@ public class FilmAction {
 		return mv;
 	}
 	
+	@RequestMapping("/add-save.jspx")
+	public ModelAndView addsave(HttpServletRequest request,Film film) {
+		ModelAndView mv = new ModelAndView("/admin/film/tip");
+		return mv;
+	}
+	
 	@RequestMapping("/delete.jspx")
 	public @ResponseBody 
 	JSONResult delete(HttpServletRequest request) {
@@ -96,6 +103,12 @@ public class FilmAction {
 	@RequestMapping("/update.jspx")
 	public ModelAndView update(HttpServletRequest request) {
 		ModelAndView mv = new ModelAndView("/admin/film/update");
+		return mv;
+	}
+	
+	@RequestMapping("/update-save.jspx")
+	public ModelAndView updatesave(HttpServletRequest request) {
+		ModelAndView mv = new ModelAndView("/admin/film/tip");
 		return mv;
 	}
 
