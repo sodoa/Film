006f17e1f9cc51003ab843536d577c673bb42164
sodoa <165614968@qq.com>
2016-07-20 22:09:15 +0800

    11111111111111111

---
+++ src/main/java/com/xinfan/wxshop/business/util/IpCache.java
@@ -0,0 +1,15 @@
+package com.xinfan.wxshop.business.util;
+
+public class IpCache {
+
+	public static String cIp = null;
+
+	public static String getcIp() {
+		return cIp;
+	}
+
+	public static void setcIp(String cIp) {
+		IpCache.cIp = cIp;
+	}
+
+}
