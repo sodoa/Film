ab872e9758488b82606b2de0b8be942a402e01f6
165614968@qq.com <165614968@qq.com>
2016-08-05 22:21:23 +0800

    11111

--- src/main/java/com/xinfan/wxshop/business/admin/AdminLoginAction.java
+++ src/main/java/com/xinfan/wxshop/business/admin/AdminLoginAction.java
@@ -1,7 +1,7 @@
 package com.xinfan.wxshop.business.admin;
 
 import javax.servlet.http.HttpServletRequest;
- 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
